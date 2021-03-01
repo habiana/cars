08d614731a9f930e5e8d0eec1dd6665647a7bfb2
habiana <79815813+habiana@users.noreply.github.com>
2021-03-01 12:32:01 +0800

    Initialize tici_record_raw

---
+++ selfdrive/loggerd/encoder.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdint>
+
+class VideoEncoder {
+public:
+  virtual ~VideoEncoder() {}
+  virtual int encode_frame(const uint8_t *y_ptr, const uint8_t *u_ptr, const uint8_t *v_ptr,
+                           int in_width, int in_height,
+                           int *frame_segment, uint64_t ts) = 0;
+  virtual void encoder_open(const char* path, int segment) = 0;
+  virtual void encoder_close() = 0;
+};
